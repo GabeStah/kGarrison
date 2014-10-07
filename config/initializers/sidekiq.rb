4ee7be665e28dc311a2e3e0e5111f1aab6e9947b
GabeStah <gwyattkelsey@gmail.com>
2014-10-06 18:06:41 -0700

    Removing sidekiq.rb initialzier.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
-Sidekiq.configure_client do |config|
-  config.redis = { :namespace => 'kgarrison', :url => 'redis://127.0.0.1:6379/0' }
-end
-
-Sidekiq.configure_server do |config|
-  config.redis = { :namespace => 'kgarrison', :url => 'redis://127.0.0.1:6379/0' }
-end
+# require 'sidekiq'
+#
+# Sidekiq.configure_client do |config|
+#   config.redis = { :namespace => 'kgarrison', :url => 'redis://127.0.0.1:6379/0' }
+# end
+#
+# Sidekiq.configure_server do |config|
+#   config.redis = { :namespace => 'kgarrison', :url => 'redis://127.0.0.1:6379/0' }
+# end
